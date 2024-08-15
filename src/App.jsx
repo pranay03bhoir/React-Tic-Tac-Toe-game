d2b364455b0df518788c6791d14eddd6236560ef
Pranay Bhoir <147631196+pranay03bhoir@users.noreply.github.com>
2024-08-15 19:43:50 +0530

    Update App.jsx
    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Player from "./components/player.jsx";
+import Player from "./components/Player.jsx";
 import GameBoard from "./components/GameBoard.jsx";
 import { useState } from "react";
 import Log from "./components/Log.jsx";
